394bfaba750e984fbc070c7cd528292d4ac11681
MSHAHID1122 <samd13894@gmail.com>
2025-04-21 12:42:36 +0500

    convert image function add form bgr to grayscale

--- file.py
+++ file.py
@@ -1,9 +1,25 @@
 import cv2
 
 
-image = r"C:\Users\User\Pictures\nature.jpg"
-img = cv2.imread(image)
-if img is None:
-    print("now image found")
-cv2.imshow("window",img)
-cv2.waitkey(0)
+# Reading the  image and printing it onto the screen
+def read_image(image):
+    
+    img = cv2.imread(image)
+    if img is None:
+        print( "no image found")
+    cv2.imshow("window",img)
+    cv2.waitKey(0)
+
+
+def convert_img(image):
+    img = cv2.imread(image)
+    if img is None:
+        return None
+    gray_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    cv2.imshow("window",gray_img)
+    cv2.waitKey(0)
+
+    
+image = r"nature.jpg"
+read_image(image)
+convert_img(image)
